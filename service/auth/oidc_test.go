e77d976b31e5b219b9800a8d77e3be1c32f680e8
Leonhard Nobach <leonhard.nobach@telekom.de>
2022-10-20 14:04:20 +0200

    OIDCHandler.groupsToDomain(..) - adapt tests to new behavior

--- service/auth/oidc_test.go
+++ service/auth/oidc_test.go
@@ -15,15 +15,15 @@ func TestGroupReplacement(t *testing.T) {
 
 	dom, valid := hut.groupsToDomain("dns3l_foo_bar")
 	assert.Equal(t, valid, true)
-	assert.Equal(t, dom, "foo.bar.")
+	assert.Equal(t, dom, "foo.bar")
 
 	dom, valid = hut.groupsToDomain("dns3l_dns3l_domain_com")
 	assert.Equal(t, valid, true)
-	assert.Equal(t, dom, "dns3l.domain.com.")
+	assert.Equal(t, dom, "dns3l.domain.com")
 
 	dom, valid = hut.groupsToDomain("dns3l_underscore__in__name_com")
 	assert.Equal(t, valid, true)
-	assert.Equal(t, dom, "underscore_in_name.com.")
+	assert.Equal(t, dom, "underscore_in_name.com")
 
 	_, valid = hut.groupsToDomain("dns3l_")
 	assert.Equal(t, valid, false)
@@ -31,6 +31,6 @@ func TestGroupReplacement(t *testing.T) {
 	hut.GroupsPrefix = ""
 	dom, valid = hut.groupsToDomain("dns3l_com")
 	assert.Equal(t, valid, true)
-	assert.Equal(t, dom, "dns3l.com.")
+	assert.Equal(t, dom, "dns3l.com")
 
 }
