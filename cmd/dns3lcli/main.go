3974fe4444c3602bce0bb8e76f0a537d783122f9
Leonhard Nobach <leo@nobach.net>
2024-06-18 10:37:00 +0200

    Removed cli because not maintained anymore (#75)

--- cmd/dns3lcli/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	cli "github.com/dns3l/dns3l-core/cli/clicmd"
-)
-
-func main() {
-	cli.Execute()
-}
--- context/context.go
+++ context/context.go
@@ -1,4 +1,3 @@
 package context
 
 var ServiceVersion string = "0.0.0"
-var CLIVersion string = "0.0.0"
