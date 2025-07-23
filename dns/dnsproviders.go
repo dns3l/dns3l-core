c97b917db5cea2e1da2baa110859fe2e7087c2ec
Leonhard Nobach <leonhard.nobach@telekom.de>
2025-07-23 10:10:20 +0200

    Added manual DNS provider

--- dns/dnsproviders.go
+++ dns/dnsproviders.go
@@ -3,6 +3,7 @@ package dns
 import (
 	"github.com/dns3l/dns3l-core/dns/bogus"
 	"github.com/dns3l/dns3l-core/dns/infblx"
+	"github.com/dns3l/dns3l-core/dns/manual"
 	"github.com/dns3l/dns3l-core/dns/otc"
 	"github.com/dns3l/dns3l-core/dns/types"
 )
@@ -11,4 +12,5 @@ var DNSProviderBuilders = map[string]func() types.DNSProviderBuilder{
 	"infoblox": func() types.DNSProviderBuilder { return &infblx.Config{} },
 	"otc":      func() types.DNSProviderBuilder { return &otc.Config{} },
 	"bogus":    func() types.DNSProviderBuilder { return &bogus.Config{} },
+	"manual":   func() types.DNSProviderBuilder { return &manual.Config{} },
 }
