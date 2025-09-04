f27637fe9e44f1590a15aa2e7f625ff9382b3891
Leonhard Nobach <leonhard.nobach@telekom.de>
2025-09-04 20:40:21 +0200

    Added DB upgrade, version bump

---
+++ db/v1.4-v1.5.0.sql
@@ -0,0 +1,34 @@
+
+/* Always do a backup / snapshot first.
+Be sure to replace the table/proc prefix
+"dns3l_" with yours if you altered the default.*/
+
+delimiter //
+
+ALTER TABLE dns3l_keycerts 
+	ADD COLUMN last_access_time TIMESTAMP DEFAULT 0 AFTER valid_end_time,
+	ADD COLUMN access_count INTEGER DEFAULT 0 AFTER last_access_time//
+
+CREATE PROCEDURE dns3l_read_increment (IN my_key_name CHAR(255), IN my_ca_id CHAR(63))
+	BEGIN
+	  UPDATE dns3l_keycerts SET last_access_time = utc_timestamp(), access_count = access_count + 1
+	  WHERE key_name = my_key_name AND ca_id = my_ca_id;
+	END//
+
+CREATE TABLE IF NOT EXISTS dns3l_renew_info (
+	renew_info TEXT
+)//
+
+CREATE PROCEDURE dns3l_set_renew_info (IN myrenew_info TEXT)
+BEGIN
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION, NOT FOUND
+    BEGIN
+    ROLLBACK;
+    END;
+    START TRANSACTION;
+    TRUNCATE TABLE dns3l_renew_info;
+    INSERT INTO dns3l_renew_info VALUES (myrenew_info);
+    COMMIT;
+END//
+
+delimiter ;
