85d3b8fb84b0c564787afa56f3de84e271aef1a6
Leonhard Nobach <leonhard.nobach@telekom.de>
2025-09-10 14:48:46 +0200

    fix: resolve chicken-egg problem with ingress

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,15 +3,23 @@ set -e
 
 export DNS3L_FQDN=${DNS3L_FQDN:-localhost}
 
-# make frontnet auth endpoint(s) available via backnet (now idempotent)
-ingress=$(getent hosts ingress | cut -f1 -d' ')
-if [ -n "$ingress" ]; then
-  entry="$ingress ${DNS3L_FQDN[*]}"
-  if ! grep "$entry" /etc/hosts >/dev/null; then
-    echo "$entry" | tee -a /etc/hosts >/dev/null
+# make frontnet auth endpoint(s) available via backnet (now idempotent).
+# retry if not immediately working.
+while true; do
+  ingress=$(getent hosts ingress | cut -f1 -d' ')
+  if [ -n "$ingress" ]; then
+    entry="$ingress ${DNS3L_FQDN[*]}"
+    if ! grep "$entry" /etc/hosts >/dev/null; then
+      echo "$entry" | tee -a /etc/hosts >/dev/null
+      echo Successfully patched hosts file with ingress IP and fqdns.
+    else
+      echo Hosts file already patched.
+    fi
+    break
+  else
+    echo Oooops. Discovering ingress IP failed. Probably the ingress is not yet up. Will retry soon.
   fi
-else
-  echo Oooops. Discovering ingress IP failed.
-fi
+  sleep 30
+done &
 
 su dns3ld -c "/run-dns3ld.sh $*" # executes the rest with dropped privileges
