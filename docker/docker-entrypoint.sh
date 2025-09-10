e993a49bb197502c645cbf09ff75092859af7614
Leonhard Nobach <leonhard.nobach@telekom.de>
2025-09-10 12:18:37 +0200

    fix: docker: ensure hosts file is patched even when restarting

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,21 +3,15 @@ set -e
 
 export DNS3L_FQDN=${DNS3L_FQDN:-localhost}
 
-# Avoid destroying bootstrapping by simple start/stop
-if [[ ! -e /.bootstrapped ]]; then
-  ### list none idempotent code blocks, here...
-
-  # make frontnet auth endpoint(s) available via backnet
-  ingress=$(getent hosts ingress | cut -f1 -d' ')
-  if [ -n "$ingress" ]; then
-    for h in ${DNS3L_FQDN}; do
-      echo "$ingress $h" | tee -a /etc/hosts >/dev/null
-    done
-  else
-    echo Oooops. Discovering ingress IP failed.
+# make frontnet auth endpoint(s) available via backnet (now idempotent)
+ingress=$(getent hosts ingress | cut -f1 -d' ')
+if [ -n "$ingress" ]; then
+  entry="$ingress ${DNS3L_FQDN[*]}"
+  if ! grep "$entry" /etc/hosts >/dev/null; then
+    echo "$entry" | tee -a /etc/hosts >/dev/null
   fi
-
-  touch /.bootstrapped
+else
+  echo Oooops. Discovering ingress IP failed.
 fi
 
 su dns3ld -c "/run-dns3ld.sh $*" # executes the rest with dropped privileges
